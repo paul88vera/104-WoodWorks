05f25b051ab3d9c77b4417be39b4ab96329e984e
paul88vera <paul88vera@gmail.com>
2024-06-01 10:54:06 -0500

    updated all design except for Shopping Cart

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,13 +1,53 @@
-import HeroBanner from "../components/HeroBanner";
-import Images from "../constants";
-
 export default function Contact() {
   return (
-    <div>
-      <HeroBanner>
-        <img src={Images.table} alt="hero" width="100%" />
-      </HeroBanner>
-      <h1 className="section-title">Contact</h1>
+    <div className="contact">
+      <div className="contrast-section" style={{ paddingTop: 0 }}>
+        <div className="contact-section">
+          <div className="contact-blurb">
+            <h1 className="section-title contact-header">
+              Questions? Need A Quote?
+            </h1>
+          </div>
+          <form className="contact-form">
+            <p>
+              Fill out the form to contact our support team directly. We will
+              get back to you as soon as we are able.
+            </p>
+            <br />
+            <label htmlFor="name">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Full Name"
+              />
+            </label>
+            <label htmlFor="phone">
+              <input
+                type="tel"
+                name="tel"
+                id="tel"
+                placeholder="Phone Number"
+              />
+            </label>
+            <label htmlFor="email">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address"
+              />
+            </label>
+            <label htmlFor="message">
+              <textarea
+                name="message"
+                id="message"
+                placeholder="How can we help you today?"></textarea>
+            </label>
+            <button className="btn submit">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
